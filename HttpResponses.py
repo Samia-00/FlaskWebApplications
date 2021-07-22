1be440b250924da1d4fb9c7c70f5168ef7d99c78
Samia-00 <samia.201315@gmail.com>
2021-07-23 00:18:02 +0600

    valid user

--- HttpResponses.py
+++ HttpResponses.py
@@ -19,5 +19,15 @@ def index2():
 	return redirect('https://github.com/Samia-00')	
 	
 	
+from flask import abort
+
+@app.route('/user/<id>')
+def get_user(id):
+	user = load_user(id)
+	if not user:
+		abort(404)
+		return '<h1>Hello, %s</h1>' % user.name
+		
+		
 if __name__ == '__main__':
       app.run(debug=True)
