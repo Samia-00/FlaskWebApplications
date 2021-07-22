09ab3ad97b85c6b39ed3cb2b59a9f7a2250962ba
Samia-00 <samia.201315@gmail.com>
2021-07-22 23:17:46 +0600

    sets a cookie

--- HttpResponses.py
+++ HttpResponses.py
@@ -5,5 +5,11 @@ app = Flask(__name__)
 def index():
        return '<h1>Bad Request</h1>', 400
 
+@app.route('/samia')
+def index1():
+	response = make_response('<h1>This document carries a cookie!</h1>')
+	response.set_cookie('answer', '42')
+	return response
+	
 if __name__ == '__main__':
       app.run(debug=True)
