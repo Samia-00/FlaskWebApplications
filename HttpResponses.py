ea70abd6bc85894f083c0806f0b78b3e6318be52
Samia-00 <samia.201315@gmail.com>
2021-07-22 23:30:41 +0600

    Redirect

--- HttpResponses.py
+++ HttpResponses.py
@@ -11,6 +11,13 @@ def index1():
 	response = make_response('<h1>This document carries a cookie!</h1>')
 	response.set_cookie('answer', '42')
 	return response
+
+
+from flask import redirect
+@app.route('/github')
+def index2():
+	return redirect('https://github.com/Samia-00')	
+	
 	
 if __name__ == '__main__':
       app.run(debug=True)
