f2390a40180035ced2467e74a1a2517921a4a55b
Samia-00 <samia.201315@gmail.com>
2021-07-23 12:42:38 +0600

    Custom Error Pages

--- Renderflask1.py
+++ Renderflask1.py
@@ -15,6 +15,9 @@ def user(name):
 	
 	return render_template('user.html', name=name)
 	
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404	
 	
 if __name__ == '__main__':
 	app.run(debug=True)
