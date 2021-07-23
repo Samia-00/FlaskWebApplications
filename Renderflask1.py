2e5168a0f651a5dfd85bdd4d734ac12bda7e42a7
Samia-00 <samia.201315@gmail.com>
2021-07-23 13:35:57 +0600

    adding random string as secret key

--- Renderflask1.py
+++ Renderflask1.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+    
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'k4jomei0sxzb680v'
 
 @app.route('/')
 @app.route('/index')
